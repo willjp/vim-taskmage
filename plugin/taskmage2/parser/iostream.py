948bb2b0ac05b8fbb898e4b22d1c2047d7666533
Will Pittman <willjpittman@gmail.com>
2018-04-20 16:10:56 -0400

    iostream.VimBuffer() tested and working.

--- plugin/taskmage2/parser/iostream.py
+++ plugin/taskmage2/parser/iostream.py
@@ -1,34 +1,15 @@
 
 # instead of this, try using io.BufferedReader( fd )
 # or io.open( fd )
+from collections import namedtuple
 
 
-
-
-class ReadStream( object ):
-    def __init__(self, fd=None):
-        pass
-
-    def __enter__(self):
-        pass
-
-    def __exit__(self):
-        pass
-
-    def next(self):
-        pass
-
-    def peek(self):
-        pass
-
-    def eof(self):
-        pass
-
-    def exception(self, msg):
-        pass
-
-
-class VimBufferStream( object ):
+class VimBuffer( object ):
+    """
+    Abstracts vim python buffer object, so can read it as if it were raw-bytes.
+    Adds functionality like `peek` , so can read ahead without changing the current
+    position in the file.
+    """
     def __init__(self, buf):
         """
         Args:
@@ -36,35 +17,94 @@ class VimBufferStream( object ):
                 A vim buffer. For example: ``vim.current.buffer`` .
         """
         self._buf  = buf
-        self._line = 0
-        self._linechar = 0
+        self.line  = -1
+        self.col   = -1
 
-    def next(self):
+    def next(self, offset=0):
         """
-        change position forwards, and retrieve the next character
-        """
-        # next char on line
-        if self._linechar < len(self._buf[ self._line ]):
-            self._linechar +=1
-            return self._buf[ self._line ][ self._linechar ]
-        # first char on next line
-        elif self._line < len(self._buf):
-            self._line     += 1
-            self._linechar  = 0
-            return self._buf[self._line][self._linechar]
+        change position forwards, and retrieves the next character
 
-    def peek(self):
+        Returns:
+
+            .. code-block:: python
+
+                'a'     # a single character
+                None    # if peek is at, or beyond the end-of-file
+
+        """
+        ch_info = self._peek()
+        if not ch_info:
+            return None
+        else:
+            self.line = ch_info.line
+            self.col  = ch_info.col
+            return ch_info.char
+
+    def peek(self, offset=0):
         """
         look at next character without changing positions
+
+        Returns:
+
+            .. code-block:: python
+
+                'a'     # a single character
+                None    # if peek is at, or beyond the end-of-file
+
         """
-        # next char on line
-        if self._linechar < len(self._buf[ self._line ]):
-            linechar = (self._linechar +1)
-            return self._buf[ self._line ][ linechar ]
-        # first char on next line
-        elif self._line < len(self._buf):
-            line     = (self._line + 1)
-            linechar = 0
-            return self._buf[line][linechar]
+        ch_info = self._peek(offset)
+        if not ch_info:
+            return None
+        else:
+            return ch_info.char
+
+    def _peek(self, offset=0):
+        """
+        Obtains character at the position of `offset` .
+
+        Returns:
+
+            .. code-block:: python
+
+                ch_info(line=1,col=5,char='b')  ## character info
+                None                            ## if reached EOF
+        """
+        ch_info = namedtuple('ch_info',['line','col','char'])
+
+        ch   = None
+        col  = self.col
+        line = self.line
+
+        # calculate offset
+        while offset >= 0:
+            if line < 0:
+                line = 0
+                col  = 0
+                ch   = self._buf[line][col]
+
+            elif col < (len(self._buf[line]) -1):
+                col+=1
+                ch = self._buf[line][col]
+
+            elif col == (len(self._buf[line]) -1):
+                col+=1
+                ch = '\n'
+
+            elif line < (len(self._buf) -1):
+                line +=1
+                col   = 0
+                ch    = self._buf[line][col]
+
+            else:
+                return None # EOF
+
+            offset -=1
+
+        return ch_info(line,col,ch)
+
+    def eof(self):
+        return self.peek() is None
+
+
 
 
