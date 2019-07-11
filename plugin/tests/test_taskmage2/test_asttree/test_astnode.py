669c65548bfc825b42be40a28cd0310cddd08ea4
Will Pittman <willjpittman@gmail.com>
2019-07-11 16:05:43 -0400

    renamed test_ast to test_asttree to match real package name

---
+++ plugin/tests/test_taskmage2/test_asttree/test_astnode.py
@@ -0,0 +1,417 @@
+import uuid
+
+import mock
+import pytest
+
+from taskmage2.asttree import astnode, nodedata
+
+ns = astnode.__name__
+
+
+class Test_Node(object):
+    def test_task_without_metadata_not_modified(self):
+        task = astnode.Node(
+            _id=None,
+            ntype='task',
+            name='task A',
+            data={
+                'status': 'todo',
+                'created': None,
+                'finished': False,
+                'modified': None,
+            },
+            children=None,
+        )
+
+        assert task.data.as_dict() == dict(
+            status='todo',
+            created=None,
+            finished=False,
+            modified=None,
+        )
+
+    def test_touch_assigns_id_if_missing(self):
+        task = astnode.Node(
+            _id=None,
+            ntype='task',
+            name='task A',
+            data={
+                'status': 'todo',
+                'created': None,
+                'finished': None,
+                'modified': None,
+            },
+            children=None,
+        )
+
+        _uuid = uuid.UUID('34db96d439164c55ac02de9173fb79ac')
+        with mock.patch('{}.uuid.uuid4'.format(ns), return_value=_uuid):
+            task.touch()
+
+        assert task.id == '34DB96D439164C55AC02DE9173FB79AC'
+
+    def test_touch_updates_children(self):
+        task = astnode.Node(
+            _id=None,
+            ntype='task',
+            name='task A',
+            data={
+                'status': 'todo',
+                'created': None,
+                'finished': None,
+                'modified': None,
+            },
+            children=[mock.Mock(), mock.Mock()],
+        )
+
+        task.touch()
+        assert all([child.touch.called for child in task.children])
+
+    def test_finalize(self):
+        print('todo')
+        assert False
+
+    def test_update_with_nonmatching_id(self):
+        params = dict(
+            ntype='task',
+            name='task A',
+            data={
+                'status': 'todo',
+                'created': None,
+                'finished': None,
+                'modified': None,
+            },
+            children=None,
+        )
+        node_A = astnode.Node(
+            _id='24B7213E055B43C2A182FB2CEDC9D36F',
+            **params
+        )
+        node_B = astnode.Node(
+            _id='7B28520767FD4EA2961A42E414022B3F',
+            **params
+        )
+        with pytest.raises(RuntimeError):
+            node_A.update(node_B)
+
+    def test_update_assigns_name(self):
+        params = dict(
+            _id=None,
+            ntype='task',
+            data={
+                'status': 'todo',
+                'created': None,
+                'finished': None,
+                'modified': None,
+            },
+            children=None,
+        )
+        node_A = astnode.Node(name='task A', **params)
+        node_B = astnode.Node(name='task B', **params)
+
+        node_A.update(node_B)
+        assert node_A.name == 'task B'
+
+    def test_update_receives_type(self):
+        params = dict(
+            _id=None,
+            ntype='task',
+            data={
+                'status': 'todo',
+                'created': None,
+                'finished': None,
+                'modified': None,
+            },
+            children=None,
+        )
+        node_A = astnode.Node(name='task A', **params)
+        node_B = astnode.Node(name='task B', **params)
+
+        # NOTE: actual value is enum, `type` returns the enum's value
+        node_A.update(node_B)
+        assert node_A.type == 'task'
+
+    def test_update_operates_on_data(self):
+        params = dict(
+            _id=None,
+            name='task A',
+            ntype='task',
+            data={
+                'status': 'todo',
+                'created': None,
+                'finished': None,
+                'modified': None,
+            },
+            children=None,
+        )
+        with mock.patch('{}.isinstance'.format(ns), return_value=True):
+            mock_data = mock.Mock(spec='{}.TaskData'.format(nodedata.__name__))
+            mock_data.update = mock.Mock()
+
+            node_A = astnode.Node(**params)
+            node_A._data = mock_data
+            node_B = astnode.Node(**params)
+
+            node_A.update(node_B)
+            assert mock_data.update.called_with(node_B)
+
+    def test_update_remove_child(self):
+        old_node = astnode.Node(
+            _id='233662903DE54C4E9FC71EF7DA2920A8',
+            ntype='task', name='task A',
+            data={
+                'status': 'todo',
+                'created': None,
+                'finished': None,
+                'modified': None,
+            },
+            children=[
+                astnode.Node(
+                    _id='AE0CC1B973694FB4B76F191B28C642FA',
+                    ntype='task', name='subtask A',
+                    data={
+                        'status': 'todo',
+                        'created': None,
+                        'finished': None,
+                        'modified': None,
+                    },
+                ),
+                astnode.Node(
+                    _id='A58ACFFF058849B291D65DFBBC146BB8',
+                    ntype='task', name='subtask B',
+                    data={
+                        'status': 'todo',
+                        'created': None,
+                        'finished': None,
+                        'modified': None,
+                    },
+                )
+            ]
+        )
+
+        new_node = astnode.Node(
+            _id='233662903DE54C4E9FC71EF7DA2920A8',
+            ntype='task', name='task A',
+            data={
+                'status': 'todo',
+                'created': None,
+                'finished': None,
+                'modified': None,
+            },
+            children=[
+                astnode.Node(
+                    _id='A58ACFFF058849B291D65DFBBC146BB8',
+                    ntype='task', name='subtask B',
+                    data={
+                        'status': 'todo',
+                        'created': None,
+                        'finished': None,
+                        'modified': None,
+                    },
+                )
+            ]
+        )
+        old_node.update(new_node)
+        assert old_node.children[0].id == new_node.children[0].id
+
+    def test_update_add_child(self):
+        old_node = astnode.Node(
+            _id='233662903DE54C4E9FC71EF7DA2920A8',
+            ntype='task', name='task A',
+            data={
+                'status': 'todo',
+                'created': None,
+                'finished': None,
+                'modified': None,
+            },
+            children=[
+                astnode.Node(
+                    _id='A58ACFFF058849B291D65DFBBC146BB8',
+                    ntype='task', name='subtask B',
+                    data={
+                        'status': 'todo',
+                        'created': None,
+                        'finished': None,
+                        'modified': None,
+                    },
+                )
+            ]
+        )
+
+        new_node = astnode.Node(
+            _id='233662903DE54C4E9FC71EF7DA2920A8',
+            ntype='task', name='task A',
+            data={
+                'status': 'todo',
+                'created': None,
+                'finished': None,
+                'modified': None,
+            },
+            children=[
+                astnode.Node(
+                    _id='AE0CC1B973694FB4B76F191B28C642FA',
+                    ntype='task', name='subtask A',
+                    data={
+                        'status': 'todo',
+                        'created': None,
+                        'finished': None,
+                        'modified': None,
+                    },
+                ),
+                astnode.Node(
+                    _id='A58ACFFF058849B291D65DFBBC146BB8',
+                    ntype='task', name='subtask B',
+                    data={
+                        'status': 'todo',
+                        'created': None,
+                        'finished': None,
+                        'modified': None,
+                    },
+                )
+            ]
+        )
+
+        old_node.update(new_node)
+        assert len(old_node.children) == 2
+        assert old_node.children[1].id == 'A58ACFFF058849B291D65DFBBC146BB8'
+
+    def test_update_name_also_updates_modified(self):
+        params = dict(
+            _id=None,
+            ntype='task',
+            data={
+                'status': 'todo',
+                'created': None,
+                'finished': None,
+                'modified': None,
+            },
+            children=None,
+        )
+        node_A = astnode.Node(name='task A', **params)
+        node_B = astnode.Node(name='task B', **params)
+
+        node_A.update(node_B)
+        assert node_A.data.modified != node_B.data.modified
+
+    def test_taskchain_completion_file(self):
+        """ files must resolve as false, at least until
+        I figure out a mechanism for archiving an entire taskfile,
+        and if that is even desirable.
+        """
+        task = astnode.Node(
+            _id=None,
+            ntype='file',
+            name='home/todo.mtask',
+            data={},
+            children=None,
+        )
+        assert task.is_complete() is False
+
+    def test_taskchain_completion_empty_section(self):
+        """ empty sections resolve as false, they are likely there
+        as markers for what is to come next.
+        """
+        task = astnode.Node(
+            _id=None,
+            ntype='section',
+            name='kitchen',
+            data={},
+            children=None,
+        )
+        assert task.is_complete() is False
+
+    def test_taskchain_completion_section_with_children(self):
+        """ entirely completed sections resolve as true.
+        """
+        task = astnode.Node(
+            _id=None,
+            ntype='section',
+            name='kitchen',
+            data={},
+            children=[
+                astnode.Node(
+                    _id=None,
+                    ntype='task',
+                    name='kitchen',
+                    data={
+                        'status': 'done',
+                        'created': None,
+                        'finished': None,
+                        'modified': None,
+                    },
+                    children=None,
+                )
+            ],
+        )
+        assert task.is_complete() is True
+
+    def test_taskchain_completion_top_task_incomplete(self):
+        task = astnode.Node(
+            _id=None,
+            ntype='task',
+            name='kitchen',
+            data={
+                'status': 'todo',
+                'created': None,
+                'finished': None,
+                'modified': None,
+            },
+            children=None,
+        )
+        assert task.is_complete() is False
+
+    def test_taskchain_completion_child_task_incomplete(self):
+        task = astnode.Node(
+            _id=None,
+            ntype='task',
+            name='kitchen',
+            data={
+                'status': 'done',
+                'created': None,
+                'finished': None,
+                'modified': None,
+            },
+            children=[
+                astnode.Node(
+                    _id=None,
+                    ntype='task',
+                    name='kitchen',
+                    data={
+                        'status': 'todo',
+                        'created': None,
+                        'finished': None,
+                        'modified': None,
+                    },
+                    children=None,
+                )
+            ],
+        )
+        assert task.is_complete() is False
+
+    def test_taskchain_completion_child_and_parent_complete(self):
+        task = astnode.Node(
+            _id=None,
+            ntype='task',
+            name='kitchen',
+            data={
+                'status': 'done',
+                'created': None,
+                'finished': None,
+                'modified': None,
+            },
+            children=[
+                astnode.Node(
+                    _id=None,
+                    ntype='task',
+                    name='kitchen',
+                    data={
+                        'status': 'skip',
+                        'created': None,
+                        'finished': None,
+                        'modified': None,
+                    },
+                    children=None,
+                )
+            ],
+        )
+        assert task.is_complete() is True
---
+++ plugin/tests/test_taskmage2/test_asttree/test_renderers.py
@@ -0,0 +1,420 @@
+#!/usr/bin/env python
+"""
+Name :          test_renderers.py
+Created :       Jul 27, 2018
+Author :        Will Pittman
+Contact :       willjpittman@gmail.com
+________________________________________________________________________________
+Description :   tests renderers.
+________________________________________________________________________________
+"""
+# builtin
+from __future__ import absolute_import, division, print_function
+import datetime
+import json
+from taskmage2.asttree import astnode, renderers
+from taskmage2.utils import timezone
+
+
+# external
+# internal
+
+
+class Test_TaskList(object):
+    def test_status_todo(self):
+        render = self.render([
+            astnode.Node(
+                _id=None,
+                ntype='task',
+                name='task A',
+                data={
+                    'status': 'todo',
+                    'created': None,
+                    'finished': False,
+                    'modified': None,
+                },
+                children=None,
+            )
+        ])
+
+        assert render == ['* task A']
+
+    def test_status_done(self):
+        render = self.render([
+            astnode.Node(
+                _id=None,
+                ntype='task',
+                name='task A',
+                data={
+                    'status': 'done',
+                    'created': None,
+                    'finished': datetime.datetime(2018, 1, 1, 0, 0, 0, tzinfo=timezone.UTC()),
+                    'modified': None,
+                },
+                children=None,
+            )
+        ])
+
+        assert render == ['x task A']
+
+    def test_status_started(self):
+        render = self.render([
+            astnode.Node(
+                _id=None,
+                ntype='task',
+                name='task A',
+                data={
+                    'status': 'wip',
+                    'created': None,
+                    'finished': False,
+                    'modified': None,
+                },
+                children=None,
+            )
+        ])
+
+        assert render == ['o task A']
+
+    def test_status_skip(self):
+        render = self.render([
+            astnode.Node(
+                _id=None,
+                ntype='task',
+                name='task A',
+                data={
+                    'status': 'skip',
+                    'created': None,
+                    'finished': False,
+                    'modified': None,
+                },
+                children=None,
+            )
+        ])
+
+        assert render == ['- task A']
+
+    def test_task_with_id(self):
+        render = self.render([
+            astnode.Node(
+                _id='5F831BF3088A42209AF644CDD8962D3A',
+                ntype='task',
+                name='task A',
+                data={
+                    'status': 'todo',
+                    'created': None,
+                    'finished': False,
+                    'modified': None,
+                },
+                children=None,
+            )
+        ])
+
+        assert render == ['*{*5F831BF3088A42209AF644CDD8962D3A*} task A']
+
+    def test_task_multiline(self):
+        render = self.render([
+            astnode.Node(
+                _id=None,
+                ntype='task',
+                name='line A\nline B\nline C',
+                data={
+                    'status': 'todo',
+                    'created': None,
+                    'finished': False,
+                    'modified': None,
+                },
+                children=None,
+            )
+        ])
+
+        assert render == [
+            '* line A',
+            '  line B',
+            '  line C',
+        ]
+
+    def test_task_multiline_with_id(self):
+        render = self.render([
+            astnode.Node(
+                _id='CB1B055F2A38477D9D944E22D3608E6A',
+                ntype='task',
+                name='line A\nline B\nline C',
+                data={
+                    'status': 'todo',
+                    'created': None,
+                    'finished': False,
+                    'modified': None,
+                },
+                children=None,
+            )
+        ])
+
+        assert render == [
+            '*{*CB1B055F2A38477D9D944E22D3608E6A*} line A',
+            '  line B',
+            '  line C',
+        ]
+
+    def test_section_without_id(self):
+        render = self.render([
+            astnode.Node(
+                _id=None,
+                ntype='section',
+                name='cleanup',
+                data={},
+                children=None,
+            )
+        ])
+
+        assert render == [
+            '',
+            'cleanup',
+            '=======',
+            '',
+        ]
+
+    def test_section_with_id(self):
+        render = self.render([
+            astnode.Node(
+                _id='E083C4632F0D41CCA4C9712F3D4BD980',
+                ntype='section',
+                name='cleanup',
+                data={},
+                children=None,
+            )
+        ])
+
+        assert render == [
+            '',
+            '{*E083C4632F0D41CCA4C9712F3D4BD980*}cleanup',
+            '=======',
+            '',
+        ]
+
+    def test_file_without_id(self):
+        render = self.render([
+            astnode.Node(
+                _id=None,
+                ntype='file',
+                name='path/projectA.mtask',
+                data={},
+                children=None,
+            )
+        ])
+
+        assert render == [
+            '',
+            'file::path/projectA.mtask',
+            '=========================',
+            '',
+        ]
+
+    def test_file_with_id(self):
+        render = self.render([
+            astnode.Node(
+                _id='E083C4632F0D41CCA4C9712F3D4BD980',
+                ntype='file',
+                name='path/projectA.mtask',
+                data={},
+                children=None,
+            )
+        ])
+
+        assert render == [
+            '',
+            '{*E083C4632F0D41CCA4C9712F3D4BD980*}file::path/projectA.mtask',
+            '=============================================================',
+            '',
+        ]
+
+    def test_section_with_tasks(self):
+        render = self.render([
+            astnode.Node(
+                _id=None,
+                ntype='section',
+                name='cleanup',
+                data={},
+                children=[
+                    astnode.Node(
+                        _id=None,
+                        ntype='task',
+                        name='subtask A',
+                        data={
+                            'status': 'todo',
+                            'created': None,
+                            'finished': False,
+                            'modified': None,
+                        },
+                        children=None,
+                    ),
+                    astnode.Node(
+                        _id=None,
+                        ntype='task',
+                        name='subtask B',
+                        data={
+                            'status': 'todo',
+                            'created': None,
+                            'finished': False,
+                            'modified': None,
+                        },
+                        children=None,
+                    ),
+                ]
+            )
+        ])
+
+        assert render == [
+            '',
+            'cleanup',
+            '=======',
+            '',
+            '* subtask A',
+            '* subtask B',
+        ]
+
+    def test_subsection_with_task_subtasks(self):
+        render = self.render([
+            astnode.Node(
+                _id=None,
+                ntype='section',
+                name='cleanup',
+                data={},
+                children=[
+                    astnode.Node(
+                        _id=None,
+                        ntype='section',
+                        name='kitchen',
+                        data={},
+                        children=[
+                            astnode.Node(
+                                _id=None,
+                                ntype='task',
+                                name='oven',
+                                data={
+                                    'status': 'todo',
+                                    'created': None,
+                                    'finished': False,
+                                    'modified': None,
+                                },
+                                children=[
+                                    astnode.Node(
+                                        _id=None,
+                                        ntype='task',
+                                        name='elements',
+                                        data={
+                                            'status': 'todo',
+                                            'created': None,
+                                            'finished': False,
+                                            'modified': None,
+                                        },
+                                        children=None,
+                                    ),
+                                ]
+                            ),
+                        ]
+                    )
+                ]
+            )
+        ])
+        print(render)
+
+        assert render == [
+            '',
+            'cleanup',
+            '=======',
+            '',
+            '',
+            'kitchen',
+            '-------',
+            '',
+            '* oven',
+            '    * elements',
+        ]
+
+    def render(self, ast):
+        """ Render parser_data using a TaskList renderer.
+
+        Args:
+            parser_data (list):
+                A list of nodes, as returned from :py:meth:`taskmage2.parser.parsers.Parser.parse` .
+
+                .. code-block:: python
+
+                    [
+                        Node('name':'home', 'type':'section', ... children=[
+                            Node('name':'laundry', ... children=[]),
+                            Node('name':'dishes', ... children=[]),
+                            ...
+                        ]),
+                        Node('name':'taskA', ...children=[])
+                    ]
+
+        Returns:
+            list:
+                A list of lines.
+        """
+        tasklist = renderers.TaskList(ast)
+        return tasklist.render()
+
+
+class Test_Mtask(object):
+    """
+    the AST is essentially the same as JSON .mtask -- not many tests needed here.
+    """
+    def test_status_todo(self):
+        render = self.render([
+            astnode.Node(
+                _id=None,
+                ntype='task',
+                name='task A',
+                data={
+                    'status': 'todo',
+                    'created': datetime.datetime(2018, 1, 1, 0, 0, 0, tzinfo=timezone.UTC()),
+                    'finished': False,
+                    'modified': datetime.datetime(2018, 1, 1, 0, 0, 0, tzinfo=timezone.UTC()),
+                },
+                children=None,
+            )
+        ])
+        assert render == [
+            {
+                '_id': None,
+                'type': 'task',
+                'name': 'task A',
+                'indent': 0,
+                'data': {
+                    'status': 'todo',
+                    'created': '2018-01-01T00:00:00+00:00',
+                    'finished': False,
+                    'modified': '2018-01-01T00:00:00+00:00',
+                },
+                'parent': None,
+            }
+        ]
+
+    def render(self, ast):
+        """ Render parser_data using a TaskList renderer.
+
+        Args:
+            parser_data (list):
+                A list of nodes, as returned from :py:meth:`taskmage2.parser.parsers.Parser.parse` .
+
+                .. code-block:: python
+
+                    [
+                        Node('name':'home', 'type':'section', ... children=[
+                            Node('name':'laundry', ... children=[]),
+                            Node('name':'dishes', ... children=[]),
+                            ...
+                        ]),
+                        Node('name':'taskA', ...children=[])
+                    ]
+
+        Returns:
+            list:
+                the parsed MTASK file as a list-of-dicts.
+        """
+        mtask = renderers.Mtask(ast)
+        mtask_str = '\n'.join(mtask.render())
+        return json.loads(mtask_str)
+
