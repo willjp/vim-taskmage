aa5f7291115ff2d6375df2252d8661acb3bb35f7
Will Pittman <willjpittman@gmail.com>
2019-07-12 19:37:16 -0400

    added failing tests for ctags

---
+++ plugin/tests/test_taskmage2/test_utils/test_ctags.py
@@ -0,0 +1,20 @@
+
+
+class Test_get_header_regex:
+    def test(self):
+        assert False
+
+
+class Test_find_header_matches:
+    def test(self):
+        assert False
+
+
+class Test_get_header_ctags_entries:
+    def test(self):
+        assert False
+
+
+class Test_get_ctags_entry:
+    def test(self):
+        assert False
