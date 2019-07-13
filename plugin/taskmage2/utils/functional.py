561714d374e03b49818d3411bf450beac405e144
Will Pittman <willjpittman@gmail.com>
2019-07-13 11:45:38 -0400

    adds new util 'functional' for functional programming utils

---
+++ plugin/taskmage2/utils/functional.py
@@ -0,0 +1,29 @@
+
+
+def multifilter(filters, result):
+    """ Applies multiple filters to `result` .
+
+    Returns:
+        list:
+            result, reduced by each filter.
+    """
+    if not filters:
+        return result
+    for f in filters:
+        result = filter(f, result)
+    return result
+
+
+if __name__ == '__main__':
+    def example():
+        def above_ten(num):
+            return num > 10
+
+        def below_fifteen(num):
+            return num < 15
+
+        filters = [above_ten, below_fifteen]
+        result = multifilter(filters, range(20))
+        print(result)
+
+    example()
