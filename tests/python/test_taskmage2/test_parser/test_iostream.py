ce4853f9c8cecd646fe7a116d832884297c3d880
Will Pittman <willjpittman@gmail.com>
2019-08-03 11:43:40 -0400

    extracts vimbuffer to it's own method

--- tests/python/test_taskmage2/test_parser/test_iostream.py
+++ tests/python/test_taskmage2/test_parser/test_iostream.py
@@ -17,93 +17,104 @@ import six
 from taskmage2.parser import iostream
 
 
+# =======
+# Helpers
+# =======
+
+
+def get_vimbuffer(contents):
+    """
+
+    Args:
+        contents (list):
+            A list of lines within the vim buffer.
+            No newline/carriage-return characters please.
+
+            .. code-block:: python
+
+                [
+                    'line 1',
+                    'line 2',
+                    ...
+                ]
+
+    """
+    buf = iostream.PureVimBuffer()
+    buf._buf = contents
+    return buf
+
+
+# =====
+# Tests
+# =====
+
+
 class Test_PureVimBuffer(object):
-    """
-    """
-    def test_peek_overflow(self):
-        buf = self.vimbuffer(['a', 'b', 'c'])
-        result = []
-        for i in range(7):
-            result.append(buf.peek(i))
-        assert result == ['a', '\n', 'b', '\n', 'c', '\n', None]
+    class Test_peek:
+        def test_peek_overflow(self):
+            buf = get_vimbuffer(['a', 'b', 'c'])
+            result = []
+            for i in range(7):
+                result.append(buf.peek(i))
+            assert result == ['a', '\n', 'b', '\n', 'c', '\n', None]
 
-    def test_peek_with_empty_line(self):
-        buf = self.vimbuffer(['', 'a', 'b', 'c'])
-        result = []
-        for i in range(8):
-            result.append(buf.peek(i))
-        assert result == ['\n', 'a', '\n', 'b', '\n', 'c', '\n', None]
+        def test_peek_with_empty_line(self):
+            buf = get_vimbuffer(['', 'a', 'b', 'c'])
+            result = []
+            for i in range(8):
+                result.append(buf.peek(i))
+            assert result == ['\n', 'a', '\n', 'b', '\n', 'c', '\n', None]
 
-    def test_peek_with_empty_lines(self):
-        buf = self.vimbuffer(['', '', '', 'a', 'b', 'c'])
-        result = []
-        for i in range(10):
-            result.append(buf.peek(i))
-        assert result == ['\n', '\n', '\n', 'a', '\n', 'b', '\n', 'c', '\n', None]
+        def test_peek_with_empty_lines(self):
+            buf = get_vimbuffer(['', '', '', 'a', 'b', 'c'])
+            result = []
+            for i in range(10):
+                result.append(buf.peek(i))
+            assert result == ['\n', '\n', '\n', 'a', '\n', 'b', '\n', 'c', '\n', None]
 
-    def test_next_overflow(self):
-        buf = self.vimbuffer(['a', 'b', 'c'])
-        result = []
-        for i in range(7):
-            result.append(buf.next())
-        assert result == ['a', '\n', 'b', '\n', 'c', '\n', None]
+        def test_next_overflow(self):
+            buf = get_vimbuffer(['a', 'b', 'c'])
+            result = []
+            for i in range(7):
+                result.append(buf.next())
+            assert result == ['a', '\n', 'b', '\n', 'c', '\n', None]
 
-    def test_peek_does_not_change_current_line_column(self):
-        buf = self.vimbuffer(['abc', 'def'])
-        buf.peek(4)
-        assert buf.col == -1
-        assert buf.line == -1
+        def test_peek_does_not_change_current_line_column(self):
+            buf = get_vimbuffer(['abc', 'def'])
+            buf.peek(4)
+            assert buf.col == -1
+            assert buf.line == -1
 
-    def test_peek_variable_line_lengths(self):
-        buf = self.vimbuffer(['abc', 'd', 'efghij'])
-        result = []
-        for i in range(14):
-            result.append(buf.peek(i))
-        assert result == ['a', 'b', 'c', '\n', 'd', '\n', 'e', 'f', 'g', 'h', 'i', 'j', '\n', None]
+        def test_peek_variable_line_lengths(self):
+            buf = get_vimbuffer(['abc', 'd', 'efghij'])
+            result = []
+            for i in range(14):
+                result.append(buf.peek(i))
+            assert result == ['a', 'b', 'c', '\n', 'd', '\n', 'e', 'f', 'g', 'h', 'i', 'j', '\n', None]
 
-    def test_peek_line_from_linestart(self):
-        buf = self.vimbuffer(['abc', 'defg'])
-        assert buf.peek_line() == 'abc'
+        def test_peek_line_from_linestart(self):
+            buf = get_vimbuffer(['abc', 'defg'])
+            assert buf.peek_line() == 'abc'
 
-    def test_peek_line_from_midline(self):
-        buf = self.vimbuffer(['abc', 'defg'])
-        buf.col = 1
-        assert buf.peek_line() == 'bc'
+        def test_peek_line_from_midline(self):
+            buf = get_vimbuffer(['abc', 'defg'])
+            buf.col = 1
+            assert buf.peek_line() == 'bc'
 
-    def test_peek_line_at_eof(self):
-        buf = self.vimbuffer(['abc', 'defg'])
-        buf.line = 1
-        buf.col = 8  # 'abc\ndefg\n'
-        assert buf.peek_line() is None
+        def test_peek_line_at_eof(self):
+            buf = get_vimbuffer(['abc', 'defg'])
+            buf.line = 1
+            buf.col = 8  # 'abc\ndefg\n'
+            assert buf.peek_line() is None
 
-    def test_peek_offset(self):
-        buf = self.vimbuffer(['abc', 'defg'])
-        assert buf.peek_line(1) == 'bc'
+        def test_peek_offset(self):
+            buf = get_vimbuffer(['abc', 'defg'])
+            assert buf.peek_line(1) == 'bc'
 
-    def test_read(self):
-        buf = self.vimbuffer(['abc', 'defg'])
-        assert buf.read() == 'abc\ndefg\n'
-
-    def vimbuffer(self, contents):
-        """
-
-        Args:
-            contents (list):
-                A list of lines within the vim buffer.
-                No newline/carriage-return characters please.
-
-                .. code-block:: python
-
-                    [
-                        'line 1',
-                        'line 2',
-                        ...
-                    ]
-
-        """
-        buf = iostream.PureVimBuffer()
-        buf._buf = contents
-        return buf
+    class Test_read:
+        def test_read(self):
+            buf = get_vimbuffer(['abc', 'defg'])
+            assert buf.read() == 'abc\ndefg\n'
 
 
 class Test_FileDescriptor(object):
